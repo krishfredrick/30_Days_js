874fe23b05c543a51dbcde59c071ba38a779a0e9
Rajesh Kannan <krishfredrick11@gmail.com>
2022-08-09 21:28:58 +0530

    Day 01 variable

---
+++ Day-1-Intro-variable/01-day-declaring/main.js
@@ -0,0 +1,4 @@
+// 
+console.log(firstName,lastName,country,city,age,havingGF)
+console.log(gravity,PI,boilingPoint)
+console.log(favHero,favMovie,favAthlete)
---
+++ Day-1-Intro-variable/01-day-declaring/variable.js
@@ -0,0 +1,14 @@
+// ----------Declaration of the variable-----------------------------//
+let firstName = "krish"
+let lastName = "fredrick"
+let country = "India"
+let city = 'Karur'
+let age = 10
+let havingGF = false;
+const gravity =9.81;
+const boilingPoint = 100;
+const PI = 3.14356
+// ---> variable can be declared in the single line using comma
+let favHero = 'Paul Walker',
+    favMovie = 'Crazy Rich Asian',
+    favAthlete = 'Rohit Sharma';
